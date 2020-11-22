aa7e310deaa2ae8fa521908931e62405a744a805
YaroslavProtsenko <YaroslavProtsenko92@gmail.com>
2020-11-22 12:21:21 +0200

    Changed let to const, renamed js.js to index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-let bread = 15.678;
-let cake = 123.965;
-let juice = 90.2345;
+const bread = 15.678;
+const cake = 123.965;
+const juice = 90.2345;
 
 console.log(Math.max(15.678, 123.965,90.2345) + ' найбільше число'); // найбільше число
 console.log(Math.min(15.678, 123.965,90.2345) + ' найменше число'); // найменше число
 
 
-let summ = bread + cake + juice; 
+const summ = bread + cake + juice; 
 
 console.log(`Сума всіх товарів ${summ}`)  // Сума всіх товарів
 
-let breadInt = Math.floor(bread);
-let cakeInt = Math.floor(cake);
-let juiceInt = Math.floor(juice);
+const breadInt = Math.floor(bread);
+const cakeInt = Math.floor(cake);
+const juiceInt = Math.floor(juice);
 
-let summInt = breadInt + cakeInt + juiceInt;
+const summInt = breadInt + cakeInt + juiceInt;
 
 console.log(`Складені цілі частини товару між собою ${summInt}`); // Складені цілі частини товару між собою
 
@@ -33,7 +33,7 @@ else {console.log('else сума всіх товарів (округлена в 
 
 console.log("Залишок після оплати", 500 - summ)
 
-let middle = Math.floor((summ/3*10))/10; // Середнє значення цін округлеен до знаку після коми.
+const middle = Math.floor((summ/3*10))/10; // Середнє значення цін округлеен до знаку після коми.
 
 console.log(`Середнє значення цін округлене до знаку після коми. ${middle}`);
 
